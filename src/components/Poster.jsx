7beb66bd9a9b6cfa6df8adefa6e0dc1a853bd692
YatharthKaushal <yatharthkaushal@proton.me>
2025-04-14 16:48:54 +0530

    Podar International School Poster added

---
+++ src/components/Poster.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import podarschool_poster from "../assets/podarschool_poster.png";
+
+const Poster = () => {
+  return (
+    <div className="sm:w-1/2 m-auto pt-8">
+      <img className="" src={podarschool_poster} alt="Podar School Poster" />
+    </div>
+  );
+};
+
+export default Poster;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import HeroSection from '../components/HeroSection';
-import ShopBy from '../components/ShopBy';
-import AboutUs from '../components/AboutUs';
-import Stores from '../components/Stores';
-import WhatWeAre from '../components/WhatWeAre';
-import AffiliatedPartners from '../components/AffiliatedPartners';
-import Footer from '../components/Footer';
-import RetailerPartners from '../components/RetailerPartners';
-import WhatsAppButton from '../components/WhatsAppButton';
-import ShopByDemandButton from '../components/ShopByDemandButton';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import ShopBy from "../components/ShopBy";
+import AboutUs from "../components/AboutUs";
+import Stores from "../components/Stores";
+import WhatWeAre from "../components/WhatWeAre";
+import AffiliatedPartners from "../components/AffiliatedPartners";
+import Footer from "../components/Footer";
+import RetailerPartners from "../components/RetailerPartners";
+import WhatsAppButton from "../components/WhatsAppButton";
+import ShopByDemandButton from "../components/ShopByDemandButton";
+import Poster from "../components/Poster";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Poster />
       <ShopBy />
       <AboutUs />
       <Stores />
